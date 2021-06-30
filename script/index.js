6d020321aa1f8688fe8c3004094d5e60e6853e66
daniszcode <danirbd13@hotmail.com>
2021-06-30 18:20:05 -0300

    Criando funcionalidade de converter a lista em objeto e em string para ser manipulada no DOM

--- script/index.js
+++ script/index.js
@@ -17,14 +17,14 @@ function addTaskStorage(task) {
     //console.log(taksString)
     const tasks = getTasksStorage(task);
     const tasksWithNewItem = [...tasks, task] // tasksWithNewItem recebe um array contendo todas as tasks já convertidas em objeto e listadas dentro de um array (...tasks) && a ultima task adicionada na lista dentro desse mesmo array
-    const tasksString = JSON.stringify(tasksWithNewItem); // transforma o array/objeto em string para ser lida no console e manipulada para aparecer na tela
+    const tasksString = JSON.stringify(tasksWithNewItem); // transforma o array/objeto em string para ser lida no localStorage(só recebe String) e manipulada para aparecer na tela
     console.log(tasksString)
     localStorage.setItem("list", tasksString);
 }
 
 function getTasksStorage() {
     const tasksString = localStorage.getItem("list"); // pegar itens da lista (tasks) e listar como um array
-    const tasks = JSON.parse(tasksString); // converte a strig em objeto  para ser lida no localStorage 
+    const tasks = JSON.parse(tasksString); // converte a strig em objeto para ser usada no Javascript
     if (!tasks) {
         return [] //Se não houver nenhuma task listada, retorne um array vazio, para salvar e criar a lista de objetos
     }
