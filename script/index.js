0f8cb680cbaebecd9fbaa2d2c172b6c61e660ae5
daniszcode <danirbd13@hotmail.com>
2021-06-30 12:05:43 -0300

    Criando documento que lista passos para criação do CRUD

---
+++ script/index.js
@@ -0,0 +1,17 @@
+document.querySelector("#form").addEventListener('submit', event => {
+event.preventDefault();
+saveTask();
+}); // esta linha de codigo faz com que o button tipo submit rode a função saveTask(), sem necessidade do onclick (substituido pelo addeventlistener(submit))
+
+function saveTask(){
+    const name = document.querySelector("#name").value;
+    //console.log(name)
+    const task = {
+        name, // poderia ser usado name:name, mas como tem o nome da mesma variavel (name), pode ser usado a virgula no lugar
+        done: false
+    };
+    //console.log(task)
+}
+
+
+
