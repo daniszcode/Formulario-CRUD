8ca53ad3158c514f4bbda0de1747ff64c95b3474
daniszcode <danirbd13@hotmail.com>
2021-07-01 10:44:50 -0300

    Lista exibida com sucesso no HTML

--- script/index.js
+++ script/index.js
@@ -1,54 +1,53 @@
+
 document.querySelector("#form").addEventListener('submit', event => {
     event.preventDefault();
     saveTask();
 }); // esta linha de codigo faz com que o button tipo submit rode a função saveTask(), sem necessidade do onclick (substituido pelo addeventlistener(submit))
 
-const listElement = document.querySelector('ul'); // tag ul será usada para tabelar a lista no HTML
-
-
+const listElement = document.querySelector('ul'); //cria a tabela para listar os itens da lista
+   
 function saveTask(){
-    const name = document.querySelector("#name").value;
-    //console.log(name)
-    const task = {
-        name, // poderia ser usado name:name, mas como tem o nome da mesma variavel (name), pode ser usado a virgula no lugar
-        done: false
-    };
-    addTaskStorage(task);
-}
-
-function addTaskStorage(task) {
-    //console.log(taksString)
-    const tasks = getTasksStorage(task);
-    const tasksWithNewItem = [...tasks, task] // tasksWithNewItem recebe um array contendo todas as tasks já convertidas em objeto e listadas dentro de um array (...tasks) && a ultima task adicionada na lista dentro desse mesmo array
-    const tasksString = JSON.stringify(tasksWithNewItem); // transforma o array/objeto em string para ser lida no localStorage(só recebe String) e manipulada para aparecer na tela
-    console.log(tasksString)
-    localStorage.setItem("list", tasksString);
-    drawList()
-}
-
-function getTasksStorage() {
-    const tasksString = localStorage.getItem("list"); // pegar/capturar lista inteira "list"
-    const tasks = JSON.parse(tasksString); // converte a strig em objeto para ser usada no Javascript
-    if (!tasks) {
-        return [] //Se não houver nenhuma task listada, retorne um array vazio, para salvar e criar a lista de objetos
+        const name = document.querySelector("#name").value;
+        //console.log(name)
+        const task = {
+            name, // poderia ser usado name:name, mas como tem o nome da mesma variavel (name), pode ser usado a virgula no lugar
+            done: false
+        };
+        addTaskStorage(task);
     }
-    return tasks;
-}
+    
+    function addTaskStorage(task) {
+        //console.log(taksString)
+        const tasks = getTasksStorage(task);
+        const tasksWithNewItem = [...tasks, task] // tasksWithNewItem recebe um array contendo todas as tasks já convertidas em objeto e listadas dentro de um array (...tasks) && a ultima task adicionada na lista dentro desse mesmo array
+        const tasksString = JSON.stringify(tasksWithNewItem); // transforma o array/objeto em string para ser lida no localStorage(só recebe String) e manipulada para aparecer na tela
+        console.log(tasksString)
+        localStorage.setItem("list", tasksString);
+        drawList()
+    }
+    
+    function getTasksStorage() {
+        const tasksString = localStorage.getItem("list"); // pegar/capturar lista inteira "list"
+        const tasks = JSON.parse(tasksString); // converte a strig em objeto para ser usada no Javascript
+        if (!tasks) {
+            return [] //Se não houver nenhuma task listada, retorne um array vazio, para salvar e criar a lista de objetos
+        }
+        return tasks;
+    }
 
+    function makeItem(item) {
+        return `
+            <li> ${item.name}</li>
+        `
 
-function makeItem(task) {
-    return `
-            <li>${task.name}</li>
-    `
-
-}
-function drawList() {
-    const tasks = getTasksStorage();
-    let item = '';
-    tasks.forEach(task => {
-        item += makeItem(task);
-    });
-    listElement.innerHTML = item
-    //console.log(item)
-}
-
+    }
+    function drawList() {
+        const tasks = getTasksStorage();
+        let item = '';
+        tasks.forEach(task => {
+            item += makeItem(task);
+        });
+        listElement.innerHTML = item
+        //console.log(item)
+    }
+    drawList()
